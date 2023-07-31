3dc6919e8c9fec58e680ef4fad7d6e7ca86bf24e
BinLi <vieri21bin@gmail.com>
2023-07-31 12:39:54 -0700

    update

--- Sources/GroceryAppSharedDTO/GroceryItemRequestDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryItemRequestDTO.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+public struct GroceryItemRequestDTO: Codable {
+  public let title: String
+  public let price: Double
+  public let quantity: Int
+  init(title: String, price: Double, quantity: Int) {
+    self.title = title
+    self.price = price
+    self.quantity = quantity
+  }
+}
--- Sources/GroceryAppSharedDTO/GroceryItemResponseDTO.swift
+++ Sources/GroceryAppSharedDTO/GroceryItemResponseDTO.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+public struct GroceryItemResponseDTO: Codable {
+  public let id: UUID
+  public let title: String
+  public let price: Double
+  public let quantity: Int
+  init(id: UUID, title: String, price: Double, quantity: Int) {
+    self.id = id
+    self.title = title
+    self.price = price
+    self.quantity = quantity
+  }
+}
